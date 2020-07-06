ff7c19a5932c259dd884a29ba2d29f528beb28ab
acomeaux93 <64748838+acomeaux93@users.noreply.github.com>
2020-07-06 11:37:44 -0400

    update

--- classwork/July_6/SuperArray.java
+++ classwork/July_6/SuperArray.java
@@ -17,6 +17,38 @@ public class SuperArray {
     this.numberElements = 0;
   }
 
+  public void append(int n){
+    int[] newArray = new int[this.data.length + 1];
+    for (int i = 0; i < this.data.length; i++){
+      newArray[i] = this.data[i];
+    }
+
+    newArray[newArray.length - 1] = n;
+    this.data = newArray;
+  }
+
+  public void add(int n){
+    for (int i = 0; i < this.data.length; i++){
+      if (this.data[i] == 0){
+        this.data[i] = n;
+        return;
+      }
+    }
+  }
+
+  public void printArray(){
+
+    for(int i = 0; i < this.data.length; i++){
+      System.out.print(this.data[i]);
+      System.out.print(" ");
+    }
+    System.out.println();
+  }
+
+  public int getSize(){
+    return this.numberElements;
+  }
+
   public static void main(String[] args) {
     SuperArray test1 = new SuperArray();
     int testArray[] = new int[5];
@@ -24,7 +56,6 @@ public class SuperArray {
 
     System.out.println(test1);
     System.out.println(test2);
-
 
   }
 
